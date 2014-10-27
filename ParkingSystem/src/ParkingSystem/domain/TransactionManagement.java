be758a832bce33482f7379bd1cf871e8ff1c3889
BhavinP <bhaveen@gmail.com>
2014-10-26 23:25:16 -0600

    Comments added for  future implementation.

--- ParkingSystem/src/ParkingSystem/domain/TransactionManagement.java
+++ ParkingSystem/src/ParkingSystem/domain/TransactionManagement.java
@@ -1,5 +1,9 @@
 package ParkingSystem.domain;
 
 public class TransactionManagement {
+	
+	//TODO: add all ticket transaction in collection for  report  ,yearly,monthly weekly calculation
+	
+	
 
 }
