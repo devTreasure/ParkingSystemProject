230eb47e76858148dc0168b086fb38474e5e947a
BhavinP <bhaveen@gmail.com>
2014-10-26 23:24:33 -0600

    Modified and refactored code.

--- ParkingSystem/src/ParkingSystem/domain/TicketManager.java
+++ ParkingSystem/src/ParkingSystem/domain/TicketManager.java
@@ -1,12 +1,17 @@
 package ParkingSystem.domain;
+import java.awt.event.ActionEvent;
+import java.util.Calendar;
 import java.util.UUID;
 
 import javax.swing.JOptionPane;
 
+import ParkingSystem.Entities.CreditCard;
 import ParkingSystem.Entities.Gate;
+import ParkingSystem.Entities.GateStatus;
 import ParkingSystem.Entities.Status;
 import ParkingSystem.Entities.Ticket;
 import ParkingSystem.Entities.TicketStatus;
+import ParkingSystem.UI.parkingGUI;
 
 
 public class TicketManager {
@@ -16,9 +21,18 @@ public class TicketManager {
 	public TicketManagement   ticketmager=new TicketManagement();
 	public PaymentManagement paymanager=new PaymentManagement();
 	public OccupancyManagement occupancy=new OccupancyManagement();
+    public FraudPrevetionManagement   fraudManager=new FraudPrevetionManagement();
+    public CreditCard   creditcard=new CreditCard();
     
-    
-    public Ticket ticket;
+    public CreditCard getCreditcard() {
+		return creditcard;
+	}
+
+	public void setCreditcard(CreditCard creditcard) {
+		this.creditcard = creditcard;
+	}
+
+	public Ticket ticket;
     public Gate gate =   new Gate(1);
     
     public Gate getGate() {
@@ -28,7 +42,11 @@ public class TicketManager {
 	public void setGate(Gate gate) {
 		this.gate = gate;
 	}
-
+	
+	public TicketManager()
+	{
+	
+	}
 	public Ticket getTicket() {
 		return ticket;
 	}
@@ -55,9 +73,15 @@ public class TicketManager {
 
 			ticket.deactivatetheTicektStatus();
 			Gate g1=gatemanagement.openExitGate(1);
+			
+			fraudManager.ticketgatecollection.put(ticket,g1);
+		    
 			occupancy.decrementOcccupancy();
 
 			Gate g2=gatemanagement.closeExitGate(1);
+			
+			fraudManager.ticketgatecollection.put(ticket,g2);
+			
 			
 			status = new Status(true, "Vehicle exited from gate.");
 			
@@ -67,9 +91,110 @@ public class TicketManager {
 		return status;
 	}
 
-	public TicketManager()
+	public void gateCloseOpeation(Gate gate) {                                         
+	    // TODO add your handling code here:
+		
+		
+	 	if(gate.gateStatus==GateStatus.Open)
+		{
+	 		gate.gateStatus=GateStatus.Close;
+		    //parkingGUI.jTextField3.setText(gate.gateStatus.toString());
+		}
+	  	    	
+		if(gate.gateStatus==GateStatus.Open)
+		{
+			gate.gateStatus=GateStatus.Close;
+		   //parkingGUI.jTextField3.setText(gate.gateStatus.toString());
+		}
+		else
+		{
+			 //parkingGUI.jTextField3.setText(gate.gateStatus.toString());
+		}
+		  
+	}
+
+	public void printTicketOperation(final parkingGUI parkingGUI) {
+		
+	        // TODO add your handling code here:
+		   
+		  
+		   
+		    final Ticket ticket = ticketmager.createTicket();
+		  
+		      ticket.activatetheTicektStatus();
+		      ticket.generateTicketID();
+		      
+		      if(ticket!=null && ticket.getTicektStatus()==TicketStatus.Active)
+		      {
+		    	  this.ticket=ticket;
+		    	 
+		    	  occupancy.incrementOcccupancy();
+		    	 
+		    	
+		    	  
+		   		 Thread ts =new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						try {
+						   Thread.sleep(2000);
+						   
+						}
+						catch (InterruptedException e) {
+	
+						}
+						
+						
+					}
+				});
+		   		
+		   		 ts.start();
+		      }
+		   		
+		   		
+		   	
+	}
+
+	public void calculateFare(parkingGUI parkingGUI) {
+		// TODO add your handling code here:
+		  Calendar   c=Calendar.getInstance();
+			
+		  java.util.Date currenttime=c.getTime();
+		
+		  ticket.setExitTime(currenttime);
+		  paymanager.setHourlyRate(11);
+		  
+		  long rate=  paymanager.calculateParkingDuration(ticket);
+		  ticket.setTicketAmount(rate);
+		
+		  
+		//  objticketmanager.gatemanagement.ExitGate(1);
+		  
+		 parkingGUI.jTextField5.setText(Long.toString(rate));
+	}
+
+	public float processPayment(Ticket ticket2) {
+	    
+		  CreditCard  crdeitcard=new CreditCard();
+		  
+		
+		  //associating ticket id to credit card id 
+		  
+		  crdeitcard.setTicketID(ticket.getTicektID());
+		
+		  paymanager.Pay(ticket);
+			
+		  
+		  float ticektAmount=ticket.getTicketAmount();
+		  
+		 return ticektAmount;
+	}
+
+	public TicketManager(parkingGUI parkingGUI)
     {
     	
     }
+
 
 }
