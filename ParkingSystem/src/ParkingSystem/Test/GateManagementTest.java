8395322bbd744ed106350cf1cc6dca644414124c
BhavinP <bhaveen@gmail.com>
2014-10-27 09:41:50 -0600

    Updated logic for gate management.

--- ParkingSystem/src/ParkingSystem/Test/GateManagementTest.java
+++ ParkingSystem/src/ParkingSystem/Test/GateManagementTest.java
@@ -8,11 +8,13 @@ import ParkingSystem.controller.GateManagement;
 import org.junit.Test;
 
 public class GateManagementTest {
+	GateManagement g = new GateManagement();
+
+	
 
 	@Test
 	public void whenGateOpeniscalledOutGateStausShoulebeOpen() {
-		GateManagement g = new GateManagement();
-
+		
 		Gate gs = g.OpenEntryGate(1);
 
 		assertEquals(gs.gateStatus, GateStatus.Open);
@@ -21,12 +23,67 @@ public class GateManagementTest {
 
 	@Test
 	public void whenGateClosediscalledOutGateStausShoulebeClosed() {
-		GateManagement g = new GateManagement();
+
 
 		Gate gs = g.closeEntryGate(1);
 
 		assertEquals(gs.gateStatus, GateStatus.Close);
 
 	}
+	
+	@Test
+	public void openEntryGate() {
+		GateManagement g = new GateManagement();
+
+		int gateID=1;
+		Gate gs = g.OpenEntryGate(gateID);
+      
+		assertEquals(gs.gateStatus, GateStatus.Open);
+
+	}
+	
+	
+	@Test
+	public void closeeEntryGate() {
+		
+		int gateID=1;
+		Gate gs = g.getGateMovementcollection().get(gateID);
+		Gate ge=g.closeEntryGate(1);
+		
+		if(gs!=null)
+		{
+		    assertEquals(gs.gateStatus, GateStatus.Open);
+		 
+		}
+		
+		 assertEquals(ge.gateStatus, GateStatus.Close);
+
+	}
+	
+	@Test
+	public void openExitGate() {
+		
+		int gateID=11;
+		Gate gs = g.openExitGate(gateID);
+      
+		assertEquals(gs.gateStatus, GateStatus.Open);
+
+	}
+	
+	@Test
+	public void closeExitGate() {
+	
+		int gateID=11;
+		Gate gs = g.closeExitGate(gateID);
+		if(gs!=null)
+		{
+		    assertEquals(gs.gateStatus, GateStatus.Open);
+		 
+		}
+      
+		assertEquals(gs.gateStatus, GateStatus.Open);
+
+	}
+
 
 }
