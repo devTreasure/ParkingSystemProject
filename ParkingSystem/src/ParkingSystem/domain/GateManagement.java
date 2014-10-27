5de6fa543e09a1d13f5ecd4a61637b1c16a8b479
BhavinP <bhaveen@gmail.com>
2014-10-26 23:23:18 -0600

    Added: new collection object to track gate operation.

--- ParkingSystem/src/ParkingSystem/domain/GateManagement.java
+++ ParkingSystem/src/ParkingSystem/domain/GateManagement.java
@@ -1,4 +1,5 @@
 package ParkingSystem.domain;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,63 +8,52 @@ import ParkingSystem.Entities.ExitGate;
 import ParkingSystem.Entities.Gate;
 import ParkingSystem.Entities.GateStatus;
 
-
 public class GateManagement {
-	
-	private Gate gate ;
-	
-	private List<Gate> gateMovementcollection  =   new ArrayList<Gate>();  
-	
-	
-	public Gate OpenEntryGate(int gateid)
-	{
-		Gate objGate=new  EntryGate(gateid);
-		this.gate=objGate;
-		this.gate.gateStatus=GateStatus.Open;
-			
-		gateMovementcollection.add(objGate);
-		
-		return objGate;
-		
-		
-	}
-	
-	public Gate closeEntryGate(int gateid)
-	{
-		Gate objGate=new  EntryGate(gateid);
-		this.gate=objGate;
-		this.gate.gateStatus=GateStatus.Close;
-			
-		gateMovementcollection.add(objGate);
-		
-		return objGate;
-		
-		
-	}
-	
-	
-	public Gate openExitGate(int gateid)
-	{
-		Gate objGate=new  ExitGate(gateid);
-		this.gate=objGate;
-		this.gate.gateStatus=GateStatus.Open;
-		
-		gateMovementcollection.add(objGate);
-		
-		return objGate;
-	}
-	
-	
-	public Gate closeExitGate(int gateid)
-	{
-		
 
-		Gate objGate=new  ExitGate( gateid);
-		this.gate=objGate;
-		this.gate.gateStatus=GateStatus.Close;
-		
+	private Gate gate;
+
+	private List<Gate> gateMovementcollection = new ArrayList<Gate>();
+
+	public Gate OpenEntryGate(int gateid) {
+		Gate objGate = new EntryGate(gateid);
+		this.gate = objGate;
+		this.gate.gateStatus = GateStatus.Open;
+
 		gateMovementcollection.add(objGate);
-		
+
+		return objGate;
+
+	}
+
+	public Gate closeEntryGate(int gateid) {
+		Gate objGate = new EntryGate(gateid);
+		this.gate = objGate;
+		this.gate.gateStatus = GateStatus.Close;
+
+		gateMovementcollection.add(objGate);
+
+		return objGate;
+
+	}
+
+	public Gate openExitGate(int gateid) {
+		Gate objGate = new ExitGate(gateid);
+		this.gate = objGate;
+		this.gate.gateStatus = GateStatus.Open;
+
+		gateMovementcollection.add(objGate);
+
+		return objGate;
+	}
+
+	public Gate closeExitGate(int gateid) {
+
+		Gate objGate = new ExitGate(gateid);
+		this.gate = objGate;
+		this.gate.gateStatus = GateStatus.Close;
+
+		gateMovementcollection.add(objGate);
+
 		return objGate;
 	}
 
