da9c542a7382cf379f6a5c84018728abaae4ef3b
parkingsystem <bhaveen@gmail.com>
2014-10-28 18:06:10 -0600

    Updated paymentmangement with credit card processing.

--- ParkingSystem/src/ParkingSystem/controller/PaymentManagement.java
+++ ParkingSystem/src/ParkingSystem/controller/PaymentManagement.java
@@ -1,117 +1,92 @@
 package ParkingSystem.controller;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import ParkingSystem.Entities.CreditCard;
 import ParkingSystem.Entities.Ticket;
 
-
 public class PaymentManagement {
+
+	private CreditCard creditCard;
+	private int parkingRate;
+
+	private int hourlyRate = 0;
+	private List<CreditCard> creditcollection = new ArrayList<CreditCard>();
+	public TransactionManagement  transactionManager = new TransactionManagement();
 	
-	private  CreditCard  creditCard;
-	private int  parkingRate;
-	
-	
-	private int hourlyRate=0;
-	private List<CreditCard> creditcollection  =   new ArrayList<CreditCard>();
-	
-	
-	
+	public PaymentManagement()
+	{
+		creditCard=new CreditCard();
+	}
+
 	public int getParkingRate() {
 		return parkingRate;
 	}
 
-
+	
+	
 	public void setParkingRate(int parkingRate) {
 		this.parkingRate = parkingRate;
 	}
-
-
 
 	public CreditCard getCreditCard() {
 		return creditCard;
 	}
 
-
-
 	public void setCreditCard(CreditCard creditCard) {
 		this.creditCard = creditCard;
 	}
-
-
 
 	public int getHourlyRate() {
 		return hourlyRate;
 	}
 
-
-
 	public void setHourlyRate(int hourlyRate) {
 		this.hourlyRate = hourlyRate;
 	}
 
+	public void processForParkingFeePayment(Ticket ticket,CreditCard  card)
 
-
-	public void Pay(Ticket ticket)
-	
 	{
-		creditcollection.add(this.creditCard);
+		creditcollection.add(card);
+		Boolean isSuccessfull= false;
 		
-	   if(ticket.getTicketAmount()>0)
-		   
-		   
-		   ticket.setTicketAmount(0);// amount paid  and due is set 0
-	       ticket.setIsPaid(true);
+		if (ticket.getTicketAmount() > 0)
+		{     
+			  card.setAmount(ticket.getTicketAmount());
+			   isSuccessfull=transactionManager.ProcessTheTransaction(card);
+		}
+			ticket.setTicketAmount(0);// amount paid and due is set 0
 		
-		//this.creditCard=objcreditCard;
+		   //Paymentmanager is only authorized to 
+		
+		    ticket.setIsPaid(true);
+
 	}
 
-	 
-	/*
-	public long calculateParkingDuration(Date datein,Date dateOut)
-	{
-		long parkingDuration=(dateOut.getTime()  -  datein.getTime());
-		long  finaltime = 0;
-		
-		if(parkingDuration>60000)
-			finaltime = (parkingDuration/60000);
-		
-		
-		return finaltime;
-		
-	}
-*/
-
-
-	public long calculateParkingDuration(Ticket ticket) {
+	public double calculateParkingDuration(Ticket ticket) {
 		// TODO Auto-generated method stub
-		
-		long parkingDuration=(ticket.getExitTime().getTime()  -  ticket.getEntryTime().getTime());
-		
-		
-		
-		//long  finaltime = 0;
-		//if(parkingDuration>60000)
-		//finaltime = (parkingDuration/60000);
-		
-		long diffSeconds = parkingDuration / 1000 % 60;
-		long diiference;
-		
-		if(parkingDuration< 60000)
-			diiference = parkingDuration / (60 * 1000) % 60;
-		else
-		
-			diiference =   ((parkingDuration / (1000*60*60)) % 24);
-		
-	    	//return random number here
-					
-		  return (long) ((2.5)  * this.hourlyRate);
-		
+
+		long parkingDuration = (ticket.getExitTime().getTime() - ticket
+				.getEntryTime().getTime());
+
+		double caluclatedTimeinSec;
+		double caluclatedTimeinMinutes;
+		double caluclatedTimeinHr;
+
+		caluclatedTimeinSec = parkingDuration / 1000;
+
+		caluclatedTimeinMinutes = caluclatedTimeinSec / 60;
+
+		caluclatedTimeinHr = caluclatedTimeinMinutes / 60;
+
+		// Stamping parking duration to the ticket
+		ticket.setParkingDuration(caluclatedTimeinHr);
+	
+
+		return ((this.hourlyRate) * ticket.getParkingDuration());
+
 	}
-	
-	
-	
-	
-	
-	
+
 }
--- ParkingSystem/src/ParkingSystem/controller/TransactionManagement.java
+++ ParkingSystem/src/ParkingSystem/controller/TransactionManagement.java
@@ -1,9 +1,88 @@
 package ParkingSystem.controller;
 
-public class TransactionManagement {
-	
-	//TODO: add all ticket transaction in collection for  report  ,yearly,monthly weekly calculation
-	
-	
+import java.util.Calendar;
 
+import javax.swing.JOptionPane;
+
+import ParkingSystem.Entities.CreditCard;
+
+public class TransactionManagement {
+
+	// TODO: add all ticket transaction in collection for report ,yearly,monthly
+	// weekly calculation
+
+	public Boolean ProcessTheTransaction(CreditCard card)
+	{
+	
+		Boolean  transactionProcessed=false;
+		
+		if(isvalidCreditCard(card) &&  card.getAmount()>0)
+		{
+	        //processing amount
+			transactionProcessed=true;
+			card.setAmount(0);
+		}
+		    
+		return transactionProcessed;
+	}
+
+	public TransactionManagement() {
+
+	}
+
+	public Boolean validateTheYear(CreditCard card) {
+		Calendar cal = Calendar.getInstance();
+		Boolean validentry = false;
+
+		int currentYear = cal.get(Calendar.YEAR);
+
+		String entermonth[] = new String[2];
+
+		try {
+			String string = card.getExpiryDate();
+			String[] parts = string.split("/");
+
+			if (parts.length > 0) {
+
+				int year = Integer.parseInt(parts[1]);
+
+				if (year < currentYear) {
+					JOptionPane.showMessageDialog(null,
+							"Please enter valid month/year.");
+				} else {
+					validentry = true;
+				}
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"Please enter the date  in mm/YYYY format.");
+			}
+		} catch (Exception e) {
+
+			JOptionPane.showMessageDialog(null,
+					"Please enter valid month/year.");
+		}
+
+		return validentry;
+	}
+
+	private Boolean isvalidCreditCard(CreditCard card) {
+
+		Boolean isverified = false;
+
+		if (card.getCCNumner().length() > 16
+				|| card.getCCNumner().length() < 16) {
+			isverified = true;
+		} else
+
+		{
+			ParkingSystemManager.errorManager
+					.setErrorMessage("Please Enter Valid Credit Card number");
+		}
+
+		if (this.validateTheYear(card)) {
+			isverified = true;
+		}
+
+		return isverified;
+	}
 }
