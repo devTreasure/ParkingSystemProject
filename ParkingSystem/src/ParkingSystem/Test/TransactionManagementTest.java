113749f08a6f8a88ba3dabdd3255f5df1ac12906
BhavinP <bhaveen@gmail.com>
2014-10-29 10:02:09 -0600

    Updated testcases for TransactionManagement

--- ParkingSystem/src/ParkingSystem/Test/TransactionManagementTest.java
+++ ParkingSystem/src/ParkingSystem/Test/TransactionManagementTest.java
@@ -11,75 +11,72 @@ public class TransactionManagementTest {
 
 	@Test
 	public void processTheTransactionWhenCreditCardIsValid() {
-	
+
 		TransactionManagement tm = new TransactionManagement();
-		 CreditCard card=new CreditCard();
-		 card.setCCNumner("1234123412341234");
-		 card.setExpiryDate("04/2015");
-		 card.setAmount(15);
-		 card.setCvvNumber(123);
-		 
+		CreditCard card = new CreditCard();
+		card.setCCNumner("1234123412341234");
+		card.setExpiryDate("04/2015");
+		card.setAmount(15);
+		card.setCvvNumber(123);
+
 		assertEquals(true, tm.ProcessTheTransaction(card));
-		 
-		 
+
 	}
-	
+
 	@Test
 	public void doNotProcessTheTransactionWhenCreditCardIsInValid() {
-	
+
 		TransactionManagement tm = new TransactionManagement();
-		 CreditCard card=new CreditCard();
-		 card.setCCNumner("1234123412341234");
-		 card.setExpiryDate("04/2012");
-		 card.setAmount(15);
-		 card.setCvvNumber(123);
-		 
+		CreditCard card = new CreditCard();
+		card.setCCNumner("1234123412341234");
+		card.setExpiryDate("04/2012");
+		card.setAmount(15);
+		card.setCvvNumber(123);
+
 		assertEquals(false, tm.ProcessTheTransaction(card));
 	}
-	
+
 	@Test
 	public void doNotProcessTheTransactionWhenCreditCardAmountIsNegative() {
-	
+
 		TransactionManagement tm = new TransactionManagement();
-		
-		 CreditCard card=new CreditCard();
-		 card.setCCNumner("1234123412341234");
-		 card.setExpiryDate("04/2014");
-		 card.setAmount(-15);
-		 card.setCvvNumber(123);
-		 
+
+		CreditCard card = new CreditCard();
+		card.setCCNumner("1234123412341234");
+		card.setExpiryDate("04/2014");
+		card.setAmount(-15);
+		card.setCvvNumber(123);
+
 		assertEquals(false, tm.ProcessTheTransaction(card));
-		
+
 	}
-	
+
 	@Test
 	public void doNotProcessTheTransactionWhenCreditCardAmountIsZero() {
-	
+
 		TransactionManagement tm = new TransactionManagement();
-		
-		 CreditCard card=new CreditCard();
-		 card.setCCNumner("1234123412341234");
-		 card.setExpiryDate("04/2014");
-		 card.setAmount(0);
-		 card.setCvvNumber(123);
-		 
+
+		CreditCard card = new CreditCard();
+		card.setCCNumner("1234123412341234");
+		card.setExpiryDate("04/2014");
+		card.setAmount(0);
+		card.setCvvNumber(123);
+
 		assertEquals(false, tm.ProcessTheTransaction(card));
 	}
-	
-	
+
 	@Test
 	public void processTheTransactionWhenAmountIsNotZero() {
-	
+
 		TransactionManagement tm = new TransactionManagement();
-		
-		 CreditCard card=new CreditCard();
-		 card.setCCNumner("1234123412341234");
-		 card.setExpiryDate("04/2015");
-		 card.setAmount(15);
-		 card.setCvvNumber(123);
-		 
+
+		CreditCard card = new CreditCard();
+		card.setCCNumner("1234123412341234");
+		card.setExpiryDate("04/2015");
+		card.setAmount(15);
+		card.setCvvNumber(123);
+
 		assertEquals(true, tm.ProcessTheTransaction(card));
 	}
-	
 
 }
